803001181324c97725c3d0cb5f9ad11cd1f35a0a
ILLUSIONack <usman-ack@hotmail.com>
2019-06-27 16:29:26 +0200

    the correct file

--- Code files/PLS.py
+++ Code files/PLS.py
@@ -1,11 +1,13 @@
 import json
 import os
-
+import csv
 
 class PublicLibrary():
     def __init__(self):
         self.catalog = Catalog()
         self.loanAdministration = LoanAdministration()
+        self.catalog.initBooks()
+        self.loanAdministration.initCustomers()
 
     def makeBackup(self):
         with open("./data/data.json", "r") as fromFile, open("./backup/" + namefile + ".json", "w") as to:
@@ -45,66 +47,85 @@ class LoanAdministration():
         pass
 
     def initCustomers(self):
-        pass
+        with open('customers.csv','r') as csv_file:
+            read = csv.reader(csv_file)
+            for line in read:
+                self.customers.append(Customer(line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10]))
+            print('customers initialised')
+            print(self.customers[4].name())
 
 
 class Person():
-    def __init__(self,gn,ns,gn,surn,ad,zip,cty,email,user,tele):
-        self.gender = gn
+    def __init__(self,gnd,ns,gn,surn,ad,zip,cty,email,user,tele):
+        self.gender = gnd
         self.nameSet = ns
         self.givenName = gn
-        self.surName = ln
+        self.surName = surn
         self.adress = ad
         self.zipCode = zip
         self.city = cty
         self.email = email
         self.userName = user
         self.telefoon = tele
+    
+    def name(self):
+        return self.givenName
 
 
 class Customer(Person):
     personid = 12345
     @staticmethod
     def gennumber():
-            A.personid +=1
-            return A.personid
+            Customer.personid +=1
+            return Customer.personid
 
-    def __init__(self,gn,ns,gn,surn,ad,zip,cty,email,user,tele):
-        super().__init__(gn,ns,gn,surn,ad,zip,cty,email,user,tele)
+    def __init__(self,gnd,ns,gn,surn,ad,zip,cty,email,user,tele):
+        super().__init__(gnd,ns,gn,surn,ad,zip,cty,email,user,tele)
         self.id = Customer.gennumber()
-        books = []
+        self.books = []
 
     def borrowBook(self,book):
-        //append to books []
         pass
     
     def returnBook(self,book):
         pass
     
     def showBorrowedBooks(self):
-        //loop array books
         pass
 
 
 class Author(Person):
-    def __init__(self):
-        super().__init__(sn,ln,ad,zip,cty,email,tele)
+    def __init__(self,gn,gnd='',ns='',surn='',ad='',zip='',cty='',email='',user='',tele=''):
+        super().__init__(gnd,ns,gn,surn,ad,zip,cty,email,user,tele)
 
-    def authorName():
-        pass
+    def authorName(self):
+        return self.givenName
 
 
 class Book():
     bookid = 12345
     @staticmethod
     def gennumber():
-            A.bookid +=1
-            return A.bookid
+            Book.bookid +=1
+            return Book.bookid
         
-    def __init__(self, title,author):
-            self.bookid = A.gennumber()
-            self.title = title
+    def __init__(self, author,country,imageLink,language,link,pages,title,year):
+            self.bookid = Book.gennumber()
             self.author = []
+            self.addAuthor(author)
+            self.country = country
+            self.imageLink = imageLink
+            self.language = language
+            self.link = link
+            self.pages = pages
+            self.title = title
+            self.year = year
+
+    def addAuthor(self,author):
+        self.author.append(Author(author))
+    
+    def name(self):
+        return self.author
 
 
 class Catalog(Book):
@@ -121,12 +142,25 @@ class Catalog(Book):
         pass
     
     def initBooks(self):
-
+        with open('bookset.json','r') as json_file:
+            read_books = json.load(json_file)
+            for x in read_books:
+                self.books.append(Book(x['author'],x['country'],x['imageLink'],x['language'],x['link'],x['pages'],x['title'],x['year']))
+            print('books initialised')
+            print(self.books[0].name()[0].authorName())
+            
+                
 
 class BookItem(Book):
     def __init__(self,title):
-        super().__init__(title,author)
+        super().__init__(author,country,imageLink,language,link,pages,title,year)
 
 
 class LoanItem():
     pass
+        
+
+if __name__ == "__main__":
+    PublicLibrary = PublicLibrary()
+
+
